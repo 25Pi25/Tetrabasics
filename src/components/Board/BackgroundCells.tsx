0f202d2c9ebb5068a74de4c01e25f7a0842bd503
25Pi25 <25pi25@mail.com>
2023-08-03 17:09:22 -0700

    removed unnecessary empty border skin

--- src/components/Board/BackgroundCells.tsx
+++ src/components/Board/BackgroundCells.tsx
@@ -1,13 +1,12 @@
 import { TilingSprite } from '@pixi/react';
 import empty from "../../assets/empty.png";
-import topBorder from "../../assets/top-border.png"
 import { Point, Texture } from 'pixi.js';
 import { Board } from './Board';
 
 export function BackgroundCells({ board }: { board: Board }) {
     return <>
         <TilingSprite
-            texture={Texture.from(topBorder)}
+            texture={Texture.from(empty)}
             width={Board.cellSize * board.width}
             height={1}
             y={Board.cellSize * (Board.matrixVisible - 1/30)}
